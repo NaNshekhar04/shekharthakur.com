79844d44f6b15f1a31d7fad59330b72dffe88cc1
NaNshekhar04 <connectshekharthakur09@gmail.com>
2023-11-10 19:31:34 +0530

    Card 1 Styles applied

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,26 @@ import Shekhar from '../app/assets/shekhar--img.jpg'
 export default function Home() {
   return (
     <main className="grid-wrapper">
-      <div className="card1">
-        <Image className="ml-5 mt-5"
+      <div className="card1 gap-7">
+        <Image className="ml-5 mt-2.4 rounded-tl-[30px] rounded-br-[30px]"
           src={Shekhar} alt={"hello"}
           width={200}
           height={200}
         />
-        <div className="myInfo">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <h1>Shekhar Thakur</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, eum.</p>
+        <div className="myInfo text-left">
+          <p className="text-[#A8ACC1]">ENGINEER, SAAS ENTHUSIAST, CRICKETER</p>
+          <h1 className="text-[#fff] font-semibold text-[36px] whitespace-normal mb-2">Shekhar Thakur.</h1>
+          <p className="text-[#9f9f9f]">I&apos;m a Full Stack Engineer by Profession and Cricketer by passion based in Mumbai.</p>
         </div>
         <div className="arrow">
-          <p>Arrow</p>
+          <div className="center-con">
+            <div className="round">
+              <div id="cta">
+                <span className="arrow primera next "></span>
+                <span className="arrow segunda next "></span>
+              </div>
+            </div>
+          </div>
         </div>
 
       </div>
